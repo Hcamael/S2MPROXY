eabb0cae8de903f0999ccbf0b06e4cd5e0509178
Hcamael <10161996+Hcamael@users.noreply.github.com>
2021-12-08 11:53:10 +0800

    V1.0

---
+++ src/python/client/udp_transport_client.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+from threading import Thread
+from queue import Queue
+import socket
+import random
+
+sSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+cSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sSock.bind(("0.0.0.0", 31500))
+cSock.bind(("0.0.0.0", 21043))
+target = [("183.158.51.90", 21044), ("183.158.53.95", 21044)]
+c_target = None
+receive_data = Queue()
+send_data = Queue()
+echo_data = Queue()
+
+def server_rv(s):
+    echo_data.put(b"start server_rv")
+    while True:
+        data = s.recvfrom(10240)
+        echo_data.put(b"server_rv get %s"%data[0])
+        receive_data.put(data[0])
+
+def server_sd(sendSock):
+    echo_data.put(b"start server_sd")
+    while True:
+        data = send_data.get()
+        echo_data.put(b"server_sd send %s"%data)
+        t = random.choice(target)
+        sendSock.sendto(data, t)
+
+def client_rv(s):
+    global c_target
+    echo_data.put(b"start client_rv")
+    while True:
+        data = s.recvfrom(10240)
+        if not c_target:
+            c_target = data[1]
+        echo_data.put(b"client_rv get %s"%data[0])
+        send_data.put(data[0])
+    
+def client_sd(s):
+    echo_data.put(b"start client_sd")
+    while True:
+        data = receive_data.get()
+        if not c_target:
+            continue
+        echo_data.put(b"client_sd send %s"%data)
+        s.sendto(data, c_target)
+
+def echo():
+    while True:
+        data = echo_data.get()
+        # print(data)
+
+def main():
+    task = [
+        Thread(target=server_rv, args=(sSock,)),
+        Thread(target=server_sd, args=(sSock,)),
+        Thread(target=client_rv, args=(cSock,)),
+        Thread(target=client_sd, args=(cSock,))
+    ]
+    for t in task:
+        t.start()
+    echo()
+
+main()
---
+++ src/python/server/udp_transport.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+from threading import Thread
+from queue import Queue
+import socket
+import random
+import sys
+
+sSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sSock.setsockopt(socket.SOL_SOCKET, 25, b"pppoe-wan\0")
+sSock_b = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sSock_b.setsockopt(socket.SOL_SOCKET, 25, b"pppoe-WAN2\0")
+cSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sSock.bind(("183.158.51.90", 21044))
+sSock_b.bind(("183.158.53.95", 21044))
+cSock.bind(("192.168.90.1", 31500))
+if len(sys.argv) == 3:
+    target = (sys.argv[1], int(sys.argv[2]))
+else:
+    target = ("192.168.90.102", 21043)
+remote_client = {}
+sendSock = [sSock, sSock_b]
+receive_data = Queue()
+send_data = Queue()
+echo_data = Queue()
+
+def server_rv(s):
+    echo_data.put(b"start server_rv")
+    global remote_client
+    while True:
+        data = s.recvfrom(10240)
+        echo_data.put(b"server_rv get %s"%data[0])
+        if s not in remote_client:
+            remote_client[s] = data[1]
+        receive_data.put(data[0])
+
+def server_sd(sendSock):
+    echo_data.put(b"start server_sd")
+    while True:
+        data = send_data.get()
+        if not remote_client:
+            continue
+        echo_data.put(b"server_sd send %s"%data)
+        while True:
+            s = random.choice(sendSock)
+            if s in remote_client:
+                break
+        s.sendto(data, remote_client[s])
+
+def client_rv(s):
+    echo_data.put(b"start client_rv")
+    while True:
+        data = s.recvfrom(10240)
+        echo_data.put(b"client_rv get %s"%data[0])
+        send_data.put(data[0])
+    
+def client_sd(s):
+    echo_data.put(b"start client_sd")
+    while True:
+        data = receive_data.get()
+        echo_data.put(b"client_sd send %s"%data)
+        s.sendto(data, target)
+
+def echo():
+    while True:
+        data = echo_data.get()
+        # print(data)
+
+def main():
+    task = [
+        Thread(target=server_rv, args=(sSock,)),
+        Thread(target=server_rv, args=(sSock_b,)),
+        Thread(target=server_sd, args=(sendSock,)),
+        Thread(target=client_rv, args=(cSock,)),
+        Thread(target=client_sd, args=(cSock,))
+    ]
+    for t in task:
+        t.start()
+    echo()
+
+main()
